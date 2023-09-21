a3f582b97c6ecc84610d7b2211fb6ccbe1e3a568
f-pulido <fpulido@csumb.edu>
2023-09-21 11:51:53 -0700

    Implemented CRUD methods

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -1,6 +1,9 @@
 package com.cst438.controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,11 +15,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.cst438.domain.Assignment;
 import com.cst438.domain.AssignmentDTO;
+import com.cst438.domain.AssignmentGrade;
+import com.cst438.domain.AssignmentGradeRepository;
 import com.cst438.domain.AssignmentRepository;
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseRepository;
@@ -51,4 +57,72 @@ public class AssignmentController {
 	}
 	
 	// TODO create CRUD methods for Assignment
+	
+	// Rest API for adding an assignment 
+	@PostMapping("/assignment/add")
+	public Assignment addAssignment(@RequestBody Assignment a) {
+		Assignment assignment = new Assignment();
+		Course course = new Course();
+		
+		// retrieve course
+		course.setCourse_id(a.getCourse().getCourse_id());
+		course.setTitle(a.getCourse().getTitle());
+		
+		courseRepository.save(course);
+		
+		// create new assignment by entering course_id, name, due date
+		
+		String pattern = "yyyy-MM-dd";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);	
+		
+		String dueDateStr = simpleDateFormat.format(a.getDueDate());
+		
+	    try {
+	        simpleDateFormat.parse(dueDateStr);
+	        assignment.setCourse(a.getCourse());
+	        assignment.setName(a.getName());
+	        assignment.setDueDate(a.getDueDate());
+	        assignmentRepository.save(assignment);
+	    } catch (ParseException e) {
+	        throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "The due date for the assignment does not have a valid format. Use yyyy-MM-dd.");
+	    }
+		return assignment;
+	}
+	
+	// Rest API for updating an assignment
+	@PutMapping("/assignment/update")
+	public Assignment updateAssignment(@RequestBody Assignment a) {
+		// Modify assignment name & due date
+		Assignment assignment = assignmentRepository.findById(a.getId()).orElse(null);
+		if (assignment != null) {
+			assignment.setName(a.getName());
+			assignment.setDueDate(a.getDueDate());
+			assignmentRepository.save(assignment);
+		} else {
+			return null;
+		}
+		return assignment;
+	}	
+	
+	// Rest API for deleting an assignment
+	@DeleteMapping("/assignment/delete")
+	public void deleteAssignment(@RequestParam("id") Integer id) {
+		
+		Assignment assignment = assignmentRepository.findById(id).orElse(null);
+		
+		
+		for (int i = 0; i < assignmentGrades.size(); i++) {
+			
+		}
+		
+		if (assignment != null) {
+			if(assignment) {
+				assignmentRepository.deleteById(id);
+			} else {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Warning...the assignment has grades.");
+			}
+		}
+		
+	}
+
 }
